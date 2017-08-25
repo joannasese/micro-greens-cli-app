2409220fb22757c2ed2a788b4b33fbf1b5522439
Joanna Sese <jmgsese@gmail.com>
2017-08-25 21:47:33 +0000

    Contributed to cli.rb

--- lib/micro_greens.rb
+++ lib/micro_greens.rb
@@ -1,9 +1,4 @@
 #Acts as environment
 
 require_relative "./micro_greens/version"
-
-# module MicroGreens
-#   # Your code goes here...
-# end
-
 require_relative './micro_greens/cli'
--- lib/micro_greens/cli.rb
+++ lib/micro_greens/cli.rb
@@ -4,6 +4,38 @@ class MicroGreens::CLI
 
   def call
     puts "Who wants to eat some salad?"
+    puts "Some examples of micro-greens include:"
+    list_greens
+    menu
+    goodbye
+  end
+
+  def menu
+    input = String.new
+    while input != "exit"
+      puts "Type in a kind of micro-green you would like to learn about. When you are done learning, type 'exit'."
+      input = gets.strip.downcase
+      case input
+      when "arugula"
+        puts "More information on arugula"
+      when "radish"
+        puts "More information on radish"
+      else
+        puts "Sorry, we don't have information on that micro-green. Try again."
+      end
+    end
+  end
+
+  def list_greens
+    puts <<-DOC
+      Arugula
+      Marigold
+      Radish
+    DOC
+  end
+
+  def goodbye
+    puts "Enjoy your salad!"
   end
 
 end
