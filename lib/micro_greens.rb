440aa3527ef1d4767b2c4f2c147877eab0137846
Joanna Sese <jmgsese@gmail.com>
2017-09-01 01:29:02 +0000

    moved contents of environment.rb to micro_greens.rb

--- lib/micro_greens.rb
+++ lib/micro_greens.rb
@@ -1,5 +1,15 @@
+#
+
+#
+# require_relative '../config/environment'
+
+require 'nokogiri'
+require 'open-uri'
+require 'colorize'
+
+require './lib/micro_greens/scraper.rb'
+require './lib/micro_greens/version'
+require './lib/micro_greens/cli'
 
 module MicroGreens
 end
-
-require_relative '../config/environment'
